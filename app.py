ae96bfc59c06e42a87ec0c3d2da93c848d3a2957
AniX2212 <136998431+AniX2212@users.noreply.github.com>
2023-06-18 22:40:42 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+<<<<<<< HEAD
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello, World!")
+Hello, World!
+=======
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello, World!")
+Hello, World!
+>>>>>>> 8dcfd5a5c15720480a25c4996abe6461aea1b6c0
---
+++ opendocker.py
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+import docx
+import os
+
+# Визначаємо шлях до файлу
+file_path = os.path.join(os.getcwd(), 'Docker Basics.docx')
+
+# Відкриваємо файл
+document = docx.Document(file_path)
+
+# Виводимо текст документу в термінал
+for paragraph in document.paragraphs:
+    print(paragraph.text)
+=======
+import docx
+import os
+
+# Визначаємо шлях до файлу
+file_path = os.path.join(os.getcwd(), 'Docker Basics.docx')
+
+# Відкриваємо файл
+document = docx.Document(file_path)
+
+# Виводимо текст документу в термінал
+for paragraph in document.paragraphs:
+    print(paragraph.text)
+>>>>>>> 8dcfd5a5c15720480a25c4996abe6461aea1b6c0
